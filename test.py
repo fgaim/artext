d61b4f70a8bb6229a61683e5ece4d9e960cbf3eb
Fitsum Gaim <fitsum@geezlab.com>
2018-10-28 17:20:38 +0900

    Add unit test

--- test.py
+++ test.py
@@ -1,33 +1,33 @@
 import sys
+import unittest
 from artext import config, utils, Artext
 
 
-if __name__ == "__main__":
-    parser = utils.arg_parser()
-    args = parser.parse_args('-src test -out test -n 5'.split() + sys.argv[1:])
+class ArtextTestCase(unittest.TestCase):
 
-    conf = config.Config()
-    conf.error_overall = args.error_rate
-    conf.path_protected_tokens = args.protected_tokens
-    conf.samples = args.samples
-    conf.separator = args.separator
+    def test_args(self):
+        parser = utils.arg_parser()
+        args = parser.parse_args('-src test -out test -n 5'.split())
+        self.assertEqual(args.source, 'test')
+        self.assertEqual(args.output, 'test')
+        self.assertEqual(args.samples, 5)
 
-    artxt = Artext(config=conf)
+    def test_noise_sentence(self):
+        conf = config.Config()
+        artxt = Artext(config=conf)
 
-    # Sentence Level
-    print('Sentence Level')
-    sent = "This person tried to keep an eye on the president while doing his work."
-    print('Input:\n{}\n'.format(sent))
-    noises = artxt.noise_sentence(sent)
-    print('Noises:')
-    for noise in noises:
-        print('-', noise)
+        # Sentence Level
+        sent = "This person tried to keep an eye on the president while doing his work."
+        noises = artxt.noise_sentence(sent)
+        self.assertNotEqual(noises, None)
+        self.assertEqual(len(noises), conf.samples)
 
-    # Document Level
-    print('\nDocument Level')
-    doc = "I went to Iceland for vacation. The top of the mountain was very cold. Fortunately, I was wearing snowboard gear."
-    print('Input:\n{}\n'.format(doc))
-    noises = artxt.noise_document(doc)
-    print('Noises:')
-    for noise in noises:
-        print('-', noise)
+    def test_noise_document(self):
+        conf = config.Config()
+        artxt = Artext(config=conf)
+
+        # Document Level
+        doc = "I went to Iceland for vacation. The top of the mountain was very cold. Fortunately, I was wearing snowboard gear."
+        noises = artxt.noise_document(doc)
+        self.assertNotEqual(noises, None)
+        self.assertEqual(len(noises), conf.samples)
