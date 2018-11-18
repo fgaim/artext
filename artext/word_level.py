74fadac64b6fa9787b7c44e9b94fbd25393e6f37
Fitsum Gaim <fitsum@geezlab.com>
2018-11-19 03:12:31 +0900

    Enhance word noising

--- artext/word_level.py
+++ artext/word_level.py
@@ -1,3 +1,4 @@
+import re
 import random
 
 from artext import utils
@@ -10,9 +11,10 @@ class WordNoiser():
     Handles word level noise injection.
     """
 
+
     def __init__(self):
-        self.lang = 'en'
-        self.keyboard = 'qwerty'
+        self.vowel_pairs = re.compile(r'([aeiou][aeiou])')
+        self.consonant_pairs = re.compile(r'(gh)|(ng)|(gn)|(th)|(ou)|(sc)')
 
 
     def noise_word(self, word):
@@ -40,14 +42,14 @@ class WordNoiser():
             rand = random.random()
             if rand <= 0.20:
                 uword = self.drop_char(uword)
-            elif rand <= 0.50:
+            elif rand <= 0.45:
                 uword = self.add_char(uword)
-            elif rand <= 0.65:
-                uword = self.swap_char(uword)
-            elif rand <= 0.75:
-                uword = self.flip_char(uword)
-            elif rand <= 0.90:
+            elif rand <= 0.55:
                 uword = self.repeat_char(uword)
+            elif rand <= 0.83:
+                uword = self.flip_chars(uword)
+            elif rand <= 0.90:
+                uword = self.swap_chars(uword)
             else:
                 pass
 
@@ -70,38 +72,13 @@ class WordNoiser():
 
     def add_char(self, word):
         """
-        Randomly add vowel to word.
+        Randomly add a vowel or a letter from a word.
         """
 
         pos = utils.rand_index(word)
         addition = random.choice(list(word+'aeiou'))
         uword = word[:pos] + addition + word[pos:]
         log.debug("[add char] {} -> {}: %s".format(word, uword))
-        return uword
-
-
-    def swap_char(self, word):
-        """
-        Randomly swap two neighboring letters in a word.
-        """
-
-        pos = utils.rand_index(word)
-        i, j = (pos - 1, pos) if pos > 0 else (pos, pos + 1)
-        uword = word[:i] + word[j] + word[i] + word[j + 1:]
-        log.debug("[swap char] {} -> {}: %s".format(word, uword))
-        return uword
-
-
-    def flip_char(self, word):
-        """
-        Randomly flip letters between two positions in a word.
-        """
-
-        i, j = utils.rand_index(word), utils.rand_index(word)
-        if i > j:
-            i, j = j, i  # swaps i and j
-        uword = word[:i] + word[j] + word[i + 1:j] + word[i] + word[j + 1:]
-        log.debug("[flip char] {} -> {}: %s".format(word, uword))
         return uword
 
 
@@ -113,4 +90,77 @@ class WordNoiser():
         pos = utils.rand_index(word)
         uword = word[:pos] + word[pos] + word[pos:]
         log.debug("[repeat char] {} -> {}: %s".format(word, uword))
+        return uword
+
+
+    def flip_chars(self, word):
+        """
+        Flip chars based on the following schemes:
+
+            1. ie, ou, iou, ae, ea
+            2. gh, th, ng
+            3. two, chars, random
+        """
+
+        uword = self.flip_vowel_pairs(word)
+        if not uword:
+            uword = self.flip_consonant_pairs(word)
+            if not uword:
+                uword = self.flip_rand_pairs(word)
+
+        log.debug("[flip char] {} -> {}: %s".format(word, uword))
+        return uword
+
+
+    def flip_vowel_pairs(self, word):
+        """
+        Flip a vowel pair in a word.
+        """
+
+        vowels = self.vowel_pairs.findall(word)
+        if not len(vowels):
+            return None
+        if len(vowels) > 1:
+            vowels = random.sample(vowels, 1)
+        pair = vowels[0]
+        uword = word.replace(pair, pair[::-1], 1)
+        return uword
+
+
+    def flip_consonant_pairs(self, word):
+        """
+        Flip a consonant pair in a word.
+        """
+
+        idxs = [m.start() for m in self.consonant_pairs.finditer(word)]
+        if not len(idxs):
+            return None
+        if len(idxs) > 1:
+            idxs = random.sample(idxs, 1)
+        idx = idxs[0]
+        uword = word[:idx] + word[idx+1] + word[idx] + (word[idx+2:] if len(word) > idx+1 else '')
+        return uword
+
+
+    def flip_rand_pairs(self, word):
+        """
+        Randomly flip two neighboring letters in a word.
+        """
+
+        pos = utils.rand_index(word)
+        i, j = (pos - 1, pos) if pos > 0 else (pos, pos + 1)
+        uword = word[:i] + word[j] + word[i] + word[j + 1:]
+        return uword
+
+
+    def swap_chars(self, word):
+        """
+        Randomly swap letters between two positions in a word.
+        """
+
+        i, j = utils.rand_index(word), utils.rand_index(word)
+        if i > j:
+            i, j = j, i  # swaps i and j
+        uword = word[:i] + word[j] + word[i + 1:j] + word[i] + word[j + 1:]
+        log.debug("[swap char] {} -> {}: %s".format(word, uword))
         return uword
