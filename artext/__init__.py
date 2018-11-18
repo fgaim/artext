228d66d0fbfab9e5baa59e151621825ce4a92a2b
Fitsum Gaim <fitsum@geezlab.com>
2018-11-18 14:16:41 +0900

    Bump version

--- artext/__init__.py
+++ artext/__init__.py
@@ -2,5 +2,5 @@ from artext import config
 from artext import utils
 from artext.artext import Artext
 
-version_info = (0, 2, 5)
+version_info = (0, 2, 6)
 __version__ = '.'.join(str(c) for c in version_info)
