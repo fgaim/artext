53d20185972218c14005ccaf2cb6e27c662aa273
Fitsum Gaim <fitsum@geezlab.com>
2018-11-29 16:48:51 +0900

    Use pytest instead of unittest

--- test_artext.py
+++ test_artext.py
@@ -1,15 +1,17 @@
-import unittest
+"""
+Unit tests for artext
+"""
 
 
-class ArtextTestCase(unittest.TestCase):
+class TestArtext():
 
     def test_args(self):
         from artext import utils
         parser = utils.arg_parser()
         args = parser.parse_args('-src test -out test -n 5'.split())
-        self.assertEqual(args.source, 'test')
-        self.assertEqual(args.output, 'test')
-        self.assertEqual(args.samples, 5)
+        assert args.source == 'test'
+        assert args.output == 'test'
+        assert args.samples == 5
 
     def test_noise_sentence(self):
         from artext import config, Artext
@@ -19,8 +21,8 @@ class ArtextTestCase(unittest.TestCase):
         # Sentence Level
         sent = "This person tried to keep an eye on the president while doing his work."
         noises = artxt.noise_sentence(sent)
-        self.assertNotEqual(noises, None)
-        self.assertEqual(len(noises), conf.samples)
+        assert noises != None
+        assert len(noises) == conf.samples
 
     def test_noise_document(self):
         from artext import config, Artext
@@ -30,8 +32,8 @@ class ArtextTestCase(unittest.TestCase):
         # Document Level
         doc = "I went to Iceland for vacation. The top of the mountain was very cold. Fortunately, I was wearing snowboard gear."
         noises = artxt.noise_document(doc)
-        self.assertNotEqual(noises, None)
-        self.assertEqual(len(noises), conf.samples)
+        assert noises != None
+        assert len(noises) == conf.samples
 
     def test(self):
         # TODO: Write proper test here
