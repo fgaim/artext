602fadd0ab22a55c66e3ea6da91fc98f357f4cf2
Fitsum Gaim <fitsum@geezlab.com>
2018-11-29 17:47:18 +0900

    Add __all__ to package init

--- artext/__init__.py
+++ artext/__init__.py
@@ -5,3 +5,8 @@ from artext.artext import Artext
 
 version_info = (0, 2, 8)
 __version__ = '.'.join(str(c) for c in version_info)
+
+__all__ = (
+    'version_info', '__version__',
+    'config', 'utils', 'Artext',
+)
