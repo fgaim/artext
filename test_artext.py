f520072427e7a91eec1c8ce365d8d2c48e0e13f3
Fitsum Gaim <fitsum@geezlab.com>
2018-11-30 01:01:38 +0900

    Clean and benchmark tests

--- test_artext.py
+++ test_artext.py
@@ -3,44 +3,41 @@ Unit tests for artext
 """
 
 
-class TestArtext():
+def test_args():
+    from artext import utils
+    parser = utils.arg_parser()
+    args = parser.parse_args('-src test -out test -n 5'.split())
+    assert args.source == 'test'
+    assert args.output == 'test'
+    assert args.samples == 5
 
-    def test_args(self):
-        from artext import utils
-        parser = utils.arg_parser()
-        args = parser.parse_args('-src test -out test -n 5'.split())
-        assert args.source == 'test'
-        assert args.output == 'test'
-        assert args.samples == 5
 
-    def test_noise_sentence(self):
-        from artext import config, Artext
-        conf = config.Config()
-        artxt = Artext(config=conf)
+def test_noise_sentence(benchmark):
+    from artext import config, Artext
+    conf = config.Config()
+    artxt = Artext(config=conf)
 
-        # Sentence Level
-        sent = "This person tried to keep an eye on the president while "
-        "doing his work."
-        noises = artxt.noise_sentence(sent)
-        assert noises is not None
-        assert len(noises) == conf.samples
+    sent = "This person tried to keep an eye on the president while doing his work."
+    noises = benchmark(artxt.noise_sentence, sent)
+    assert noises is not None
+    assert len(noises) == conf.samples
 
-    def test_noise_document(self):
-        from artext import config, Artext
-        conf = config.Config()
-        artxt = Artext(config=conf)
 
-        # Document Level
-        doc = "I went to Iceland for vacation. The top of the mountain was "
-        "very cold. Fortunately, I was wearing snowboard gear."
-        noises = artxt.noise_document(doc)
-        assert noises is not None
-        assert len(noises) == conf.samples
+def test_noise_document(benchmark):
+    from artext import config, Artext
+    conf = config.Config()
+    artxt = Artext(config=conf)
 
-    def test_word_noise(self):
-        from artext.word_level import WordNoiser
-        wnoiser = WordNoiser()
-        word = 'Mountain'
-        uword = wnoiser.noise_word(word)
-        assert uword is not None
-        assert uword != word
+    doc = "I went to Iceland for vacation. The top of the mountain was very cold."
+    noises = benchmark(artxt.noise_document, doc)
+    assert noises is not None
+    assert len(noises) == conf.samples
+
+
+def test_word_noise(benchmark):
+    from artext.word_level import WordNoiser
+    wnoiser = WordNoiser()
+    word = 'Mountain'
+    uword = benchmark(wnoiser.noise_word, word)
+    assert uword is not None
+    assert uword != word
